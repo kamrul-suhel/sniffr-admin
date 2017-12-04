5292bd77cf9a8da422f23204f273f8e2fb782af3
Michael Wright <mike@unilad.xco.uk>
2017-12-04 16:33:42 +0000

    - Facebook video playback - More video fixes

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -525,6 +525,16 @@
 	</script>
 	<!-- End Notifications -->
 
+	<!-- Tracking -->
+	<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.11&appId=151068855526504';
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	<!-- End Notifications -->
+
 	@yield('javascript')
 	@yield('javascript-videojs')
 
