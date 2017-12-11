6503a4d8cd2d3eaf5a7dbd4575f9f72d92558b0b
Michael Wright <mike@unilad.xco.uk>
2017-12-11 12:08:54 +0000

    Forgot to remove the date displayed for testing

--- public/content/themes/default/partials/video-loop.php
+++ public/content/themes/default/partials/video-loop.php
@@ -23,7 +23,7 @@ foreach($videos as $video):
 			</div>
 		</a>
 		<div class="block-contents">
-			<p class="desc"><?php echo $video->licensed_at; ?>|<?php if(strlen($video->description) > 90){ echo substr($video->description, 0, 90) . '...'; } else { echo $video->description; } ?></p>
+			<p class="desc"><?php if(strlen($video->description) > 90){ echo substr($video->description, 0, 90) . '...'; } else { echo $video->description; } ?></p>
 		</div>
 	</article>
 </div>
