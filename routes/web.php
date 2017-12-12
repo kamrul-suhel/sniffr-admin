0431d3aecb2b43a72a1c379dc804b0628634c60a
Michael Wright <mike@unilad.xco.uk>
2017-12-12 11:15:19 +0000

    Fixed routes conflict

--- routes/web.php
+++ routes/web.php
@@ -161,7 +161,6 @@ Route::group(array('prefix' => 'admin'), function(){
     Route::get('', 'Admin\AdminController@index');
 
     // Admin Video Functionality
-<<<<<<< HEAD
     Route::get('admin/videos', 'AdminVideosController@index');
     Route::get('admin/videos/edit/{id}', 'AdminVideosController@edit');
     Route::post('admin/videos/update', array('uses' => 'AdminVideosController@update'));
@@ -179,7 +178,6 @@ Route::group(array('prefix' => 'admin'), function(){
     Route::get('admin/videos/statusapi/{state}/{id}', array('uses' => 'AdminVideosController@statusapi')); //test for ajax call
     Route::get('admin/videos/remind/{id}', array('uses' => 'AdminVideosController@remind'));
     Route::post('admin/videos/comment/{id}', array('uses' => 'AdminVideosController@comment'));
-
 
     Route::get('admin/posts', 'AdminPostController@index');
     Route::get('admin/posts/create', 'AdminPostController@create');
@@ -260,104 +258,6 @@ Route::group(array('prefix' => 'admin'), function(){
     Route::get('admin/theme_settings_form', 'AdminThemeSettingsController@theme_settings_form');
     Route::get('admin/theme_settings', 'AdminThemeSettingsController@theme_settings');
     Route::post('admin/theme_settings', array('uses' => 'AdminThemeSettingsController@update_theme_settings'));
-=======
-    Route::resource('videos', 'Admin\AdminVideosController');
-    Route::get('videos/delete/{id}', array('uses' => 'Admin\AdminVideosController@destroy'));
-    Route::post('videos/comment/{id}', array('uses' => 'Admin\AdminVideosController@comment'));
-    
-    Route::get('videos/{id}', array('uses' => 'Admin\AdminVideosController@index'));
-    Route::get('videos/status/{state}/{id}', array('uses' => 'Admin\AdminVideosController@status'));
-    Route::get('videos/remind/{id}', array('uses' => 'Admin\AdminVideosController@remind'));
-    Route::post('videos/comment/{id}', array('uses' => 'Admin\AdminVideosController@comment'));
-
-
-    Route::get('videos/categories', 'Admin\AdminVideoCategoriesController@index');
-    Route::post('videos/categories/store', array('uses' => 'Admin\AdminVideoCategoriesController@store'));
-    Route::post('videos/categories/order', array('uses' => 'Admin\AdminVideoCategoriesController@order'));
-    Route::get('videos/categories/edit/{id}', 'Admin\AdminVideoCategoriesController@edit');
-    Route::post('videos/categories/update', array('uses' => 'Admin\AdminVideoCategoriesController@update'));
-    Route::get('videos/categories/delete/{id}', array('uses' => 'Admin\AdminVideoCategoriesController@destroy'));
-
-    Route::get('posts', 'Admin\AdminPostController@index');
-    Route::get('posts/create', 'Admin\AdminPostController@create');
-    Route::post('posts/store', array('uses' => 'Admin\AdminPostController@store'));
-    Route::get('posts/edit/{id}', 'Admin\AdminPostController@edit');
-    Route::post('posts/update', array('uses' => 'Admin\AdminPostController@update'));
-    Route::get('posts/delete/{id}', array('uses' => 'Admin\AdminPostController@destroy'));
-    Route::get('posts/categories', 'Admin\AdminPostCategoriesController@index');
-    Route::post('posts/categories/store', array('uses' => 'Admin\AdminPostCategoriesController@store'));
-    Route::post('posts/categories/order', array('uses' => 'Admin\AdminPostCategoriesController@order'));
-    Route::get('posts/categories/edit/{id}', 'Admin\AdminPostCategoriesController@edit');
-    Route::get('posts/categories/delete/{id}', array('uses' => 'Admin\AdminPostCategoriesController@destroy'));
-    Route::post('posts/categories/update', array('uses' => 'Admin\AdminPostCategoriesController@update'));
-
-    Route::get('media', 'Admin\AdminMediaController@index');
-    Route::post('media/files', 'Admin\AdminMediaController@files');
-    Route::post('media/new_folder', 'Admin\AdminMediaController@new_folder');
-    Route::post('media/delete_file_folder', 'Admin\AdminMediaController@delete_file_folder');
-    Route::get('media/directories', 'Admin\AdminMediaController@get_all_dirs');
-    Route::post('media/move_file', 'Admin\AdminMediaController@move_file');
-    Route::post('media/upload', 'Admin\AdminMediaController@upload');
-
-    Route::get('pages', 'Admin\AdminPageController@index');
-    Route::get('pages/create', 'Admin\AdminPageController@create');
-    Route::post('pages/store', array('uses' => 'Admin\AdminPageController@store'));
-    Route::get('pages/edit/{id}', 'Admin\AdminPageController@edit');
-    Route::post('pages/update', array('uses' => 'Admin\AdminPageController@update'));
-    Route::get('pages/delete/{id}', array('uses' => 'Admin\AdminPageController@destroy'));
-
-    Route::get('clients', 'Admin\AdminClientController@index');
-    Route::get('clients/create', 'Admin\AdminClientController@create');
-    Route::post('clients/store', array('uses' => 'Admin\AdminClientController@store'));
-    Route::get('clients/edit/{id}', 'Admin\AdminClientController@edit');
-    Route::post('clients/update', array('uses' => 'Admin\AdminClientController@update'));
-    Route::get('clients/delete/{id}', array('uses' => 'Admin\AdminClientController@destroy'));
-
-    Route::get('contacts', 'Admin\AdminContactController@index');
-    Route::get('contacts/create', 'Admin\AdminContactController@create');
-    Route::post('contacts/store', array('uses' => 'Admin\AdminContactController@store'));
-    Route::get('contacts/edit/{id}', 'Admin\AdminContactController@edit');
-    Route::post('contacts/update', array('uses' => 'Admin\AdminContactController@update'));
-    Route::get('contacts/delete/{id}', array('uses' => 'Admin\AdminContactController@destroy'));
-
-    Route::get('campaigns', 'Admin\AdminCampaignController@index');
-    Route::get('campaigns/create', 'Admin\AdminCampaignController@create');
-    Route::post('campaigns/store', array('uses' => 'Admin\AdminCampaignController@store'));
-    Route::get('campaigns/edit/{id}', 'Admin\AdminCampaignController@edit');
-    Route::post('campaigns/update', array('uses' => 'Admin\AdminCampaignController@update'));
-    Route::get('campaigns/delete/{id}', array('uses' => 'Admin\AdminCampaignController@destroy'));
-
-    Route::get('users', 'Admin\AdminUsersController@index');
-    Route::get('user/create', 'Admin\AdminUsersController@create');
-    Route::post('user/store', array('uses' => 'Admin\AdminUsersController@store'));
-    Route::get('user/edit/{id}', 'Admin\AdminUsersController@edit');
-    Route::post('user/update', array('uses' => 'Admin\AdminUsersController@update'));
-    Route::get('user/delete/{id}', array('uses' => 'Admin\AdminUsersController@destroy'));
-
-    Route::get('menu', 'Admin\AdminMenuController@index');
-    Route::post('menu/store', array('uses' => 'Admin\AdminMenuController@store'));
-    Route::get('menu/edit/{id}', 'Admin\AdminMenuController@edit');
-    Route::post('menu/update', array('uses' => 'Admin\AdminMenuController@update'));
-    Route::post('menu/order', array('uses' => 'Admin\AdminMenuController@order'));
-    Route::get('menu/delete/{id}', array('uses' => 'Admin\AdminMenuController@destroy'));
-
-    Route::get('plugins', 'Admin\AdminPluginsController@index');
-    Route::get('plugin/deactivate/{plugin_name}', 'Admin\AdminPluginsController@deactivate');
-    Route::get('plugin/activate/{plugin_name}', 'Admin\AdminPluginsController@activate');
-
-    Route::get('themes', 'Admin\AdminThemesController@index');
-    Route::get('theme/activate/{slug}', array('uses' => 'Admin\AdminThemesController@activate'));
-
-    Route::get('settings', 'Admin\AdminSettingsController@index');
-    Route::post('settings', array('uses' => 'Admin\AdminSettingsController@save_settings'));
-
-    Route::get('payment_settings', 'Admin\AdminPaymentSettingsController@index');
-    Route::post('payment_settings', array('uses' => 'Admin\AdminPaymentSettingsController@save_payment_settings'));
-
-    Route::get('theme_settings_form', 'Admin\AdminThemeSettingsController@theme_settings_form');
-    Route::get('theme_settings', 'Admin\AdminThemeSettingsController@theme_settings');
-    Route::post('theme_settings', array('uses' => 'Admin\AdminThemeSettingsController@update_theme_settings'));
->>>>>>> c6489494bf56d6a42dc4085b7032c29ac63ff10f
 });
 
 /*
