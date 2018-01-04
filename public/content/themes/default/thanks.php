078c73fdb3878d74ba3fb5b836fe554d29bdd5cb
Michael Wright <mike@unilad.xco.uk>
2018-01-04 12:41:13 +0000

    - Fixed up 404 page - Updated thanks page - Updated more info thanks page - Added to style css plus hamster image

--- public/content/themes/default/thanks.php
+++ public/content/themes/default/thanks.php
@@ -1,10 +1,10 @@
 <?php include('includes/header.php'); ?>
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-12 page">
-			<div class="panel-heading">Thanks for submitting your video</div>
-		</div>
+<div class="container bg-404">
+	<div class="area-404">
+		<h1>Thanks for the video.. You rock!</h1>
+		<img src="/content/themes/default/assets/img/hamster_thanks.png" class="hamster_thanks" border="0" />
+		<div class="clear"></div>
 	</div>
 </div>
 
