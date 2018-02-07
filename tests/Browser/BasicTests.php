5ba0727b922cbf0d04def789ebf9d1b9e2507840
Michael Wright <mike@unilad.xco.uk>
2018-02-07 10:23:40 +0000

    Update while working on Unit/Dusk tests (includes 2 x upload form, 2 x non-ex submission form, admin video search, admin video accepted tests

--- tests/Browser/BasicTests.php
+++ tests/Browser/BasicTests.php
@@ -3,138 +3,74 @@
 namespace Tests\Browser;
 
 use App\User;
+use App\Video;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\Browser\Pages\LoginPage;
+use Tests\Browser\Components\FillFormUpload;
+use Tests\Browser\Components\FillFormSubmission;
 
 class BasicTests extends DuskTestCase
 {
-    public function testLoginPageExists()
+    public function testUploadVideoLink() // Tests upload form for video link
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                    ->assertSee('Please Login');
+        $this->browse(function ($browser) {
+            $browser->visit('/upload')
+                ->within(new FillFormUpload, function ($browser) {
+                    $browser->enterTestData('link');
+                })
+                ->type('url', 'https://www.youtube.com/watch?v=qRHPA3LawQI')
+                ->within(new FillFormUpload, function ($browser) {
+                    $browser->executeUpload();
+                })
+                ->screenshot('success-'.$this->getName().'-'.time());
         });
     }
-    //
-    // public function testLoginAdminManualCredentials()
-    // {
-    //     $this->browse(function ($browser) {
-    //         $browser->visit('/login')
-    //             ->type('email', 'mike@unilad.co.uk')
-    //             ->type('password', 'removed-password-after-testing')
-    //             ->press('Sign in')
-    //             ->assertPathIs('/admin');
-    //     });
-    // }
 
-    // public function testUploadVideoLink() // Tests upload form for video link
-    // {
-    //     $this->browse(function ($browser) {
-    //         $browser->visit('/upload')
-    //         ->resize(1560, 1500)
-    //                 ->assertSee('Your Contact Details')
-    //                 ->type('full_name', 'Mike Wright')
-    //                 ->type('email', 'mike@unilad.co.uk')
-    //                 ->type('temp-tel', '07782497004')
-    //                 ->type('title', 'TEST video link from unit browser '.time())
-    //                 ->type('url', 'https://www.youtube.com/watch?v=qRHPA3LawQI')
-    //                 ->click('p.terms-copy')
-    //                 ->press('Submit your video')
-    //                 ->assertSee('just uploading your awesome video')
-    //                 ->waitForLocation('/thanks', 10)
-    //                 // ->waitForText('Thanks for the video', 10)
-    //                 ->screenshot('success-uploadlink-'.$this->getName().'-'.time());
-    //     });
-    // }
+    public function testUploadVideoFile() // Tests upload form for video file
+    {
+        $this->browse(function ($browser) {
+            $browser->visit('/upload')
+                ->within(new FillFormUpload, function ($browser) {
+                    $browser->enterTestData('file');
+                })
+                ->attach('file', public_path('/assets/unit_test_assets/test_small.mp4'))
+                ->within(new FillFormUpload, function ($browser) {
+                    $browser->executeUpload();
+                })
+                ->screenshot('success-'.$this->getName().'-'.time());
+        });
+    }
 
-    // public function testUploadVideoFile() // Tests upload form for video file
-    // {
-    //     $this->browse(function ($browser) {
-    //         $browser->visit('/upload')
-    //         ->resize(1560, 1500)
-    //                 ->assertSee('Your Contact Details')
-    //                 ->type('full_name', 'Mike Wright')
-    //                 ->type('email', 'mike@unilad.co.uk')
-    //                 ->type('temp-tel', '07782497004')
-    //                 ->type('title', 'TEST video file from unit browser '.time())
-    //                 ->attach('file', public_path('/assets/unit_test_assets/test_small.mp4'))
-    //                 ->click('p.terms-copy')
-    //                 ->press('Submit your video')
-    //                 ->assertSee('just uploading your awesome video')
-    //                 ->waitForLocation('/thanks', 10)
-    //                 // ->waitForText('Thanks for the video', 10)
-    //                 ->screenshot('success-uploadvideo-'.$this->getName().'-'.time());
-    //     });
-    // }
-
-    // public function testSubmissionVideoLink() // Tests non exclusive submission form for video link
-    // {
-    //     $this->browse(function ($browser) {
-    //         $browser->visit('/submission')
-    //         ->resize(1560, 1500)
-    //                 ->assertSee('Your Contact Details')
-    //                 ->type('full_name', 'Mike Wright')
-    //                 ->type('email', 'mike@unilad.co.uk')
-    //                 ->type('title', 'TEST non-ex video link from unit browser '.time())
-    //                 ->type('url', 'https://www.youtube.com/watch?v=qRHPA3LawQI')
-    //                 ->type('notes', 'something')
-    //                 ->type('credit', 'www.unilad.co.uk')
-    //                 ->type('referrer', 'Adam')
-    //                 ->click('p.terms-copy')
-    //                 ->press('Submit your video')
-    //                 ->assertSee('just uploading your awesome video')
-    //                 ->waitForLocation('/thanks', 10)
-    //                 // ->waitForText('Thanks for the video', 10)
-    //                 ->screenshot('success-submissionlink-'.$this->getName().'-'.time());
-    //     });
-    // }
+    public function testSubmissionVideoLink() // Tests non exclusive submission form for video link
+    {
+        $this->browse(function ($browser) {
+            $browser->visit('/submission')
+                ->within(new FillFormSubmission, function ($browser) {
+                    $browser->enterTestData('link');
+                })
+                ->type('url', 'https://www.youtube.com/watch?v=qRHPA3LawQI')
+                ->within(new FillFormSubmission, function ($browser) {
+                    $browser->executeUpload();
+                })
+                ->screenshot('success-'.$this->getName().'-'.time());
+        });
+    }
 
     public function testSubmissionVideoFile() // Tests non exclusive submission form for video file
     {
         $this->browse(function ($browser) {
             $browser->visit('/submission')
-            ->resize(1560, 1500)
-                    ->assertSee('Your Contact Details')
-                    ->type('full_name', 'Mike Wright')
-                    ->type('email', 'mike@unilad.co.uk')
-                    ->type('title', 'TEST non-ex video file from unit browser '.time())
-                    ->attach('file', public_path('/assets/unit_test_assets/test_small.mp4'))
-                    ->type('notes', 'something')
-                    ->type('credit', 'www.unilad.co.uk')
-                    ->type('referrer', 'Adam')
-                    ->click('p.terms-copy')
-                    ->press('Submit your video')
-                    ->assertSee('just uploading your awesome video')
-                    ->waitForLocation('/thanks', 10)
-                    // ->waitForText('Thanks for the video', 10)
-                    ->screenshot('success-submissionfile-'.$this->getName().'-'.time());
+                ->within(new FillFormSubmission, function ($browser) {
+                    $browser->enterTestData('file');
+                })
+                ->attach('file', public_path('/assets/unit_test_assets/test_small.mp4'))
+                ->within(new FillFormSubmission, function ($browser) {
+                    $browser->executeUpload();
+                })
+                ->screenshot('success-'.$this->getName().'-'.time());
         });
     }
-
-    // public function testAdminLogin() // Tests login using Admin user
-    // {
-    //     $this->browse(function ($browser) {
-    //         $browser->on(new LoginPage)
-    //                 ->loginUser()
-    //                 ->visit('/admin')
-    //                 ->assertPathIs('/admin')
-    //                 ->assertSee('Howdy, Mikewright');
-    //     });
-    // }
-    //
-    // public function testAdminVideosLicensed() // Tests admin video section
-    // {
-    //     $this->browse(function ($browser) {
-    //         $browser->visit('/admin/videos/licensed')
-    //                 ->assertSee('Licensed Videos')
-    //                 ->type('s', 'Little kid shits himself')
-    //                 ->keys('#search-input', '{enter}')
-    //                 ->assertSee('Little kid shits himself')
-    //                 ->resize(1560, 1000)
-    //                 ->screenshot('success-loginadmin-'.$this->getName().'-'.time());
-    //     });
-    // }
 
 }
--- tests/Browser/Pages/Page.php
+++ tests/Browser/Pages/Page.php
@@ -6,15 +6,10 @@ use Laravel\Dusk\Page as BasePage;
 
 abstract class Page extends BasePage
 {
-    /**
-     * Get the global element shortcuts for the site.
-     *
-     * @return array
-     */
     public static function siteElements()
     {
         return [
-            '@element' => '#selector',
+            '@terms' => 'p.terms-copy',
         ];
     }
 }
