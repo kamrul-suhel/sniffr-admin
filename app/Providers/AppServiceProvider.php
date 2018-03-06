24bd4bf946836e7f55fa5f78cea7a91fccf586f8
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-03-06 12:31:50 +0000

    Begaining Frontend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Facebook\Facebook;
 
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
     }
 
     /**
