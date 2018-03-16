ea51205cd35ad1aeb135737d84910ae27809d0da
Michael Wright <mike@unilad.xco.uk>
2018-03-16 11:20:15 +0000

    - Downloaded latest LIVE database - Added Google Tag manager code to template footer

--- public/content/themes/default/includes/footer.php
+++ public/content/themes/default/includes/footer.php
@@ -80,13 +80,21 @@
 	</script>
 
 	<?php if(isset($settings->google_tracking_id) && $settings->google_tracking_id != ''): ?>
+	<style>.async-hide { opacity: 0 !important} </style>
+	<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+	h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+	(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+	})(window,document.documentElement,'async-hide','dataLayer',4000,
+	{'GTM-P4P3PWK':true});</script>
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', '<?= $settings->google_tracking_id ?>', 'auto');
+		ga('require', 'GTM-P4P3PWK');
 		ga('send', 'pageview');
+
 	</script>
 	<?php endif; ?>
 	<script>(function(d, s, id) {
