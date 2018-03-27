940105c54f8be9034a1361ea62f24cc7ee851084
Francisco Perez <frank@unilad.co.uk>
2018-03-27 22:47:10 +0100

    Post controller swap posts and index methods + CS

--- app/Http/Controllers/ThemePostController.php
+++ app/Http/Controllers/ThemePostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Auth;
 use Redirect;
-
 use App\User;
 use App\Post;
 use App\Page;
@@ -12,67 +11,53 @@ use App\Menu;
 use App\Setting;
 use App\VideoCategory;
 use App\PostCategory;
-
 use App\Libraries\ThemeHelper;
-
 use Illuminate\Support\Facades\Input;
 
-class ThemePostController extends Controller {
-
+class ThemePostController extends Controller
+{
     private $posts_per_page = 12;
 
     public function __construct()
     {
-        //$this->middleware('secure');
         $settings = Setting::first();
         $this->posts_per_page = $settings->posts_per_page;
     }
 
     /**
-     * Display the specified post.
-     *
-     * @param  int  $id
-     * @return Response
+     * @param $slug
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
      */
-    public function index($slug)
+    public function show($slug)
     {
         $post = Post::where('slug', '=', $slug)->first();
-        
-        //Make sure post is active
-        if((!Auth::guest() && Auth::user()->role == 'admin') || $post->active){
 
+        if ((!Auth::guest() && Auth::user()->role == 'admin') || $post->active) {
             $author = User::find($post->user_id);
-            $data = array(
-                    'post' => $post, 
-                    'author' => $author,
-                    'menu' => Menu::orderBy('order', 'ASC')->get(),
-                    'video_categories' => VideoCategory::all(),
-                    'post_categories' => PostCategory::all(),
-                    'theme_settings' => ThemeHelper::getThemeSettings(),
-                    'pages' => Page::where('active', '=', 1)->get(),
-                );
+            $data = [
+                'post' => $post,
+                'author' => $author,
+                'menu' => Menu::orderBy('order', 'ASC')->get(),
+                'video_categories' => VideoCategory::all(),
+                'post_categories' => PostCategory::all(),
+                'theme_settings' => ThemeHelper::getThemeSettings(),
+                'pages' => Page::where('active', '=', 1)->get(),
+            ];
             return view('Theme::post', $data);
-
-        } else {
-            return Redirect::to('posts')->with(array('note' => 'Sorry, this post is no longer active.', 'note_type' => 'error'));
         }
+
+        return Redirect::to('posts')->with(array('note' => 'Sorry, this post is no longer active.', 'note_type' => 'error'));
     }
 
 
-    /*
-     * Page That shows the latest posts list
-     *
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function posts()
-    {   
-        $page = Input::get('page');
-        if( !empty($page) ){
-            $page = Input::get('page');
-        } else {
-            $page = 1;
-        }
+    public function index()
+    {
+        $page = Input::get('page', 1);
 
-        $data = array(
+        $data = [
             'posts' => Post::where('active', '=', '1')->orderBy('created_at', 'DESC')->simplePaginate($this->posts_per_page),
             'current_page' => $page,
             'page_title' => 'All Posts',
@@ -83,22 +68,21 @@ class ThemePostController extends Controller {
             'post_categories' => PostCategory::all(),
             'theme_settings' => ThemeHelper::getThemeSettings(),
             'pages' => Page::where('active', '=', 1)->get(),
-            );
+        ];
 
         return view('Theme::post-list', $data);
     }
 
+    /**
+     * @param $category
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function category($category)
     {
-        $page = Input::get('page');
-        if( !empty($page) ){
-            $page = Input::get('page');
-        } else {
-            $page = 1;
-        }
+        $page = Input::get('page', 1);
 
         $cat = PostCategory::where('slug', '=', $category)->first();
-        $data = array(
+        $data = [
             'posts' => Post::where('active', '=', '1')->where('post_category_id', '=', $cat->id)->orderBy('created_at', 'DESC')->simplePaginate($this->posts_per_page),
             'current_page' => $page,
             'category' => $cat,
@@ -110,10 +94,8 @@ class ThemePostController extends Controller {
             'post_categories' => PostCategory::all(),
             'theme_settings' => ThemeHelper::getThemeSettings(),
             'pages' => Page::where('active', '=', 1)->get(),
-        );
+        ];
 
         return view('Theme::post-list', $data);
     }
-
-
 }
