0ab448429dee0a8407decbb0c581119ccb072325
Francisco Perez <frank@unilad.co.uk>
2018-03-27 11:43:13 +0100

    Add phpDocs and CS to Controller, BaseController, PasswordController

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -1,39 +1,38 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
-use Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
-use Auth;
-use User;
-use Redirect;
+use App\User;
 
-class PasswordController extends Controller {
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reset Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller is responsible for handling password reset requests
-	| and uses a simple trait to include this behavior. You're free to
-	| explore this trait and override any methods you wish to tweak.
-	|
-	*/
-	use ResetsPasswords;
-	/**
-	 * Create a new password controller instance.
-	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\PasswordBroker  $passwords
-	 * @return void
-	 */
-	public function __construct(Guard $auth, PasswordBroker $passwords)
-	{
-		$this->auth = $auth;
-		$this->passwords = $passwords;
-		$this->middleware('guest');
-	}
+class PasswordController extends Controller
+{
+    use ResetsPasswords;
+
+    /**
+     * @var Guard
+     */
+    private $auth;
+
+    /**
+     * @var PasswordBroker
+     */
+    private $passwords;
+
+    /**
+     * Create a new password controller instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Guard $auth
+     * @param  \Illuminate\Contracts\Auth\PasswordBroker $passwords
+     * @return void
+     */
+    public function __construct(Guard $auth, PasswordBroker $passwords)
+    {
+        $this->auth = $auth;
+        $this->passwords = $passwords;
+        $this->middleware('guest');
+    }
 }
