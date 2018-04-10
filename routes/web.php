2412d36aa1d7a04d4370a72a3407432228b677ca
Ian Lainchbury <>
2018-04-10 18:44:50 +0100

    Emergency route update and removing csrf requirement on upload form (for facebook form)

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::group(array('before' => 'if_logged_in_must_be_subscribed'), function(){
     */
     Route::post('upload', 'ThemeUploadController@store');
     Route::get('upload', 'ThemeUploadController@index');
-    Route::get('upload/form', 'ThemeUploadController@form');
+    Route::any('upload/form', 'ThemeUploadController@form');
     Route::post('issue', 'ThemeUploadController@issueAlert');
     Route::post('videocheck', 'ThemeUploadController@videoCheck');
     //Route::view('videocheckform', 'frontend.test');
