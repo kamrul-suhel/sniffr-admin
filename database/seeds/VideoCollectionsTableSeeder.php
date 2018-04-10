15cf6f9096738c3d3367630765466660cf8fa680
Francisco Perez <frank@unilad.co.uk>
2018-04-10 22:05:45 +0100

    Seeders: Video, VideoCategories, VideoCollections, VideoShotTypes

---
+++ database/seeds/VideoCollectionsTableSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class VideoCollectionsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        \DB::table('video_collections')->insert([
+            0 => [
+                'parent_id' => NULL,
+                'order' => 1,
+                'name' => 'Animals',
+                'slug' => 'animals',
+            ],
+            1 => [
+                'parent_id' => NULL,
+                'order' => 2,
+                'name' => 'New Content',
+                'slug' => 'new-content',
+            ],
+            2 => [
+                'parent_id' => NULL,
+                'order' => 3,
+                'name' => 'Fails',
+                'slug' => 'fails',
+            ],
+            3 => [
+                'parent_id' => NULL,
+                'order' => 4,
+                'name' => 'News/Newsworthy',
+                'slug' => 'newsworthy',
+            ],
+            4 => [
+                'parent_id' => NULL,
+                'order' => 5,
+                'name' => 'Funny/Humour',
+                'slug' => 'funny',
+            ],
+            5 => [
+                'parent_id' => NULL,
+                'order' => 6,
+                'name' => 'Feel Good',
+                'slug' => 'feel-good',
+            ],
+            6 => [
+                'parent_id' => NULL,
+                'order' => 7,
+                'name' => 'Sports',
+                'slug' => 'sports',
+            ],
+            7 => [
+                'parent_id' => NULL,
+                'order' => 8,
+                'name' => 'Weather',
+                'slug' => 'weather',
+            ],
+            8 => [
+                'parent_id' => NULL,
+                'order' => 9,
+                'name' => 'Cool Stuff',
+                'slug' => 'cool-stuff',
+            ],
+            9 => [
+                'parent_id' => NULL,
+                'order' => 10,
+                'name' => 'Archive',
+                'slug' => 'archive',
+            ],
+        ]);
+    }
+}
---
+++ database/seeds/VideosTableSeeder.php
@@ -0,0 +1,78 @@
+<?php
+
+use App\Contact;
+use App\Video;
+use App\VideoCategory;
+use App\VideoCollection;
+use App\VideoShotType;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class VideosTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+        $contactIds = Contact::lists('id');
+        $videoCategoryIds = VideoCategory::lists('id');
+        $videoCollectionIds = VideoCollection::lists('id');
+        $videoShotTypeIds = VideoShotType::lists('id');
+        $states = env('sites.states');
+
+        foreach (range(1, 30) as $index) {
+            Video::create([
+                'alpha_id' => sha1(microtime()),
+                'state' => $faker->randomElement($states),
+                'maybe' => NULL,
+                'user_id' => NULL,
+                'contact_id' => $faker->randomElement($contactIds),
+                'video_category_id' => $faker->randomElement($videoCategoryIds),
+                'video_collection_id' => $faker->randomElement($videoCollectionIds),
+                'video_shottype_id' => $faker->randomElement($videoShotTypeIds),
+                'title' => $faker->sentence,
+                'rights' => 'ex',
+                'access' => 'guest',
+                'details' => NULL,
+                'description' => NULL,
+                'notes' => NULL,
+                'nsfw' => NULL,
+                'referrer' => NULL,
+                'credit' => NULL,
+                'active' => 1,
+                'featured' => 0,
+                'views' => 0,
+                'image' => '',
+                'thumb' => '',
+                'mime' => NULL,
+                'ext' => NULL,
+                'url' => 'https://www.reddit.com/r/aww/comments/7d55y7/whenever_i_stop_petting_my_brittany/',
+                'file' => NULL,
+                'file_watermark' => NULL,
+                'file_watermark_dirty' => NULL,
+                'link' => NULL,
+                'vertical' => NULL,
+                'youtube_id' => '',
+                'embed_code' => '',
+                'duration' => NULL,
+                'date_filmed' => NULL,
+                'location' => NULL,
+                'source' => NULL,
+                'more_details' => NULL,
+                'more_details_sent' => NULL,
+                'more_details_code' => NULL,
+                'reminders' => NULL,
+                'contact_is_owner' => NULL,
+                'submitted_elsewhere' => NULL,
+                'submitted_where' => NULL,
+                'allow_publish' => NULL,
+                'filmed_by_me' => NULL,
+                'permission' => NULL,
+                'is_exclusive' => NULL,
+                'terms' => NULL,
+                'ip' => NULL,
+                'user_agent' => NULL,
+                'licensed_at' => NULL,
+            ]);
+        }
+    }
+}
