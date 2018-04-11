98f37d0f8a7ca97658c988ab0e57bc6be0d0f7c9
Francisco Perez <frank@unilad.co.uk>
2018-04-11 16:16:31 +0100

    Add Tag Table Seeder

---
+++ database/seeds/TagVideoTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Video;
+use App\Tag;
+use Faker\Factory as Faker;
+
+class TagVideoTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+        $tagIds = Tag::lists('id');
+        $videoIds = Video::lists('id');
+        $unique_ids = [];
+
+        foreach (range(1, 300) as $index) {
+            // check composite id is unique
+            $newVideoId = $faker->randomElement($videoIds);
+            $newTagId = $faker->randomElement($tagIds);
+            $new_composite_id = $newVideoId . $newTagId;
+
+            if (key_exists($new_composite_id, $unique_ids)) {
+                continue;
+            }
+
+            $unique_ids[$new_composite_id] = 1;
+            \DB::table('tag_video')->insert([
+                [
+                    'video_id' => $newVideoId,
+                    'tag_id' => $newTagId,
+                ],
+            ]);
+        }
+    }
+}
