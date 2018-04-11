2401eaf4bf5cb67f49a8448783deab282d8eec2a
Francisco Perez <frank@unilad.co.uk>
2018-04-11 10:47:22 +0100

    Fix call to site configuration in Campaign Seeder

--- database/seeds/CampaignVideoTableSeeder.php
+++ database/seeds/CampaignVideoTableSeeder.php
@@ -13,16 +13,18 @@ class CampaignVideoTableSeeder extends Seeder
         $campaignIds = Contact::lists('id');
         $videoIds = Video::lists('id');
         $unique_ids = [];
+        $states = env('site.states');
+
         foreach (range(1, 30) as $index) {
+            // check composite id is unique
             $newVideoId = $faker->randomElement($videoIds);
             $newCampaignId = $faker->randomElement($campaignIds);
-            $states = env('sites.states');
-
-            $new_id = $newVideoId . $newCampaignId;
-            if (key_exists($new_id, $unique_ids)) {
+            $new_composite_id = $newVideoId . $newCampaignId;
+            if (key_exists($new_composite_id, $unique_ids)) {
                 continue;
             }
-            $unique_ids[$new_id] = 1;
+
+            $unique_ids[$new_composite_id] = 1;
 
             DB::table('campaign_video')->insert([
                 'video_id' => $newVideoId,
