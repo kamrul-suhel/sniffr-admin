b14b590937d12bc5ccbd58cd79fb4340c5cbd6bd
Francisco Perez <frank@unilad.co.uk>
2018-04-11 16:08:04 +0100

    Create Verticals list in a config file

---
+++ config/verticals.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    0 => [
+        'name' => 'UNILAD',
+    ],
+    1 => [
+        'name' => 'Gaming',
+    ],
+    2 => [
+        'name' => 'Adventure',
+    ],
+    3 => [
+        'name' => 'Fitness',
+    ],
+    4 => [
+        'name' => 'Grub',
+    ],
+    5 => [
+        'name' => 'Film',
+    ],
+    6 => [
+        'name' => 'Tech',
+    ],
+    7 => [
+        'name' => 'Sound',
+    ],
+    8 => [
+        'name' => 'Sensitive',
+    ],
+    9 => [
+        'name' => 'Editorial',
+    ],
+];
