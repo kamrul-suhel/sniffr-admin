d120793f7d02ea53b0a2d96e352d504438d52fae
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-04-21 21:10:06 +0100

    Home page latest video

--- app/Http/Controllers/ThemeHomeController.php
+++ app/Http/Controllers/ThemeHomeController.php
@@ -7,40 +7,27 @@ use App\Menu;
 use App\Video;
 use App\VideoCategory;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class ThemeHomeController extends Controller
 {
-    /**
-     * @var int
-     */
-    private $videos_per_page;
 
     public function __construct()
     {
-        $settings = config('settings.site');
-        $this->videos_per_page = $settings['videos_per_page'];
+    
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $total_video = Video::all()->count();
 
-        $current_month_upload_video = Video::where('created_at', '>=', Carbon::now()->startOfMonth())->count();
-        $active_video = Video::where('active', 1)->count();
-
-        $data = [
-            'total_video' => $total_video,
-            'current_month_upload_video' => $current_month_upload_video,
-            'active_video'  => $active_video,
-            'videos' => Video::where('state', 'licensed')->orderBy('created_at', 'DESC')->simplePaginate($this->videos_per_page),
-            'current_page' => 1,
-            'menu' => Menu::orderBy('order', 'ASC')->get(),
-            'pagination_url' => '/videos',
-            'video_categories' => VideoCategory::all(),
-            'theme_settings' => config('settings.theme'),
-            'pages' => Page::where('active', '=', 1)->get(),
-        ];
-
-		return view('frontend.master',$data);
+        if($request->ajax()){
+            $data = [
+                'videos' => Video::where('state', 'licensed')
+                       ->orderBy('created_at', 'DESC')
+                        ->limit(12)
+                        ->get()
+            ];
+        }
+		return view('frontend.master');
 	}
 }
