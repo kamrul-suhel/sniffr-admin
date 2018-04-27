40b6bf8fafa5a89147246c907f15b385fc601ac3
Francisco Perez <frank@unilad.co.uk>
2018-04-27 11:25:32 +0100

    Youtube Access Token removed We need to fake the Youtube Integration or create a dev account

--- database/seeds/YoutubeAccessTableSeeder.php
+++ database/seeds/YoutubeAccessTableSeeder.php
@@ -16,7 +16,7 @@ class YoutubeAccessTableSeeder extends Seeder
 
         \DB::table('youtube_access_tokens')->insert([
             [
-                'access_token' => 'ya29.Gl2nBefoVnCXvox0TSmppbH8eTe5f6OFgs9nUH6omMQfTUySfHml9ycCkxaMuA5quBadAy73U9_LlwoMcd_qnuCYIsufx8eALCI7g5t4uAr6-PUVJigMsaHHED9ZBIs',
+                'access_token' => '',
                 'created_at' => $faker->dateTime(),
             ],
         ]);
