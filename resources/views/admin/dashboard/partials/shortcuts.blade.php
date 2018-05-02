a2b132cf2f01e7f121bb61ad202958adf4b340e7
Francisco Perez <frank@unilad.co.uk>
2018-05-02 14:32:52 +0100

    Add a shortcut in the dashboard to add new videos

--- resources/views/admin/dashboard/partials/shortcuts.blade.php
+++ resources/views/admin/dashboard/partials/shortcuts.blade.php
@@ -1,3 +1,19 @@
-<a href="/">
-    New Story Submission
-</a>
+
+<div class="row">
+    <div class="col-sm-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="panel-title">Shortcuts</div>
+            </div>
+
+            <div class="panel-body">
+                <div class="tab-content">
+                    <a href="{{ url('admin/videos/create') }}" class="btn btn-success">
+                        <i class="fa fa-plus-circle"></i>
+                        Add New Video
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
