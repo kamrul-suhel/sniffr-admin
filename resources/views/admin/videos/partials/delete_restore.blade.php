284ba86aac47001eb002d4aad6093ec9994c7452
Francisco Perez <frank@unilad.co.uk>
2018-05-03 14:02:26 +0100

    Remove unnecessary if condition

--- resources/views/admin/videos/partials/delete_restore.blade.php
+++ resources/views/admin/videos/partials/delete_restore.blade.php
@@ -1,9 +1,9 @@
-@if(($video) && ($video->trashed()))
+@if($video->trashed())
     <a href="{{ url('admin/videos/restore/'.$video->alpha_id) }}" title="Restore Video" class="btn btn-warning">
         <i class="fa fa-fa-upload"></i>
         Restore
     </a>
-@elseif($video)
+@else
     <a href="{{ url('admin/videos/delete/' . $video->alpha_id) }}" title="Delete Video" class="btn btn-danger">
         <i class="fa fa-trash-o"></i>
         Delete
