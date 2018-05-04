a6ac432a8df7a18b9657585bfa28c816d321bcb7
Francisco Perez <frank@unilad.co.uk>
2018-05-04 16:40:35 +0100

    Add condition to check if more_details_sent is a date or null

--- resources/views/admin/videos/partials/license.blade.php
+++ resources/views/admin/videos/partials/license.blade.php
@@ -29,54 +29,64 @@
 
     <div class="panel-footer">
         <div class="text-right">
-            @if($video->state == 'pending'||$video->state == 'problem'||$video->state == 'licensed'||$video->state=='restricted')
+            @if($video->state == 'pending' || $video->state == 'problem' || $video->state == 'licensed' || $video->state=='restricted')
                 @if($video->state != 'licensed')
-                    <a href="{{ url('admin/videos/status/licensed/'.$video->alpha_id ) }}"
-                       class="btn btn-primary btn-success">License</a>
+                    <a href="{{ url('admin/videos/status/licensed/' . $video->alpha_id ) }}" class="btn btn-primary btn-success">
+                        License
+                    </a>
                 @endif
                 @if($video->state != 'restricted')
-                    <a href="{{ url('admin/videos/status/restricted/'.$video->alpha_id ) }}"
-                       class="btn btn-primary btn-warning">Restricted</a>
+                    <a href="{{ url('admin/videos/status/restricted/' . $video->alpha_id ) }}" class="btn btn-primary btn-warning">
+                        Restricted
+                    </a>
                 @endif
                 @if($video->state != 'problem')
-                    <a href="{{ url('admin/videos/status/problem/'.$video->alpha_id ) }}"
-                       class="btn btn-primary btn-danger">Problem</a>
+                    <a href="{{ url('admin/videos/status/problem/' . $video->alpha_id ) }}" class="btn btn-primary btn-danger">
+                        Problem
+                    </a>
                 @endif
             @elseif($video->state == 'new')
-                <a href="{{ url('admin/videos/status/accepted/'.$video->alpha_id ) }}"
-                   class="btn btn-primary btn-success js-state-accept">Accept</a>
-                <a href="{{ url('admin/videos/status/rejected/'.$video->alpha_id ) }}"
+                <a href="{{ url('admin/videos/status/accepted/' . $video->alpha_id ) }}" class="btn btn-primary btn-success js-state-accept">
+                    Accept
+                </a>
+                <a href="{{ url('admin/videos/status/rejected/' . $video->alpha_id ) }}"
                    class="btn btn-primary btn-danger">Reject</a>
             @elseif($video->state == 'accepted')
                 <div class="pull-left">
                     @if($video->reminders)
                         Reminder {{ $video->reminders }}
-                        Sent: {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$video->more_details_sent)->diffForHumans() }}
-                        <a href="{{ url('admin/videos/remind/'.$video->alpha_id ) }}"
-                           class="btn btn-primary btn-danger">Send Reminder</a>
+                        Sent: {{ ($video->more_details_sent) ? \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $video->more_details_sent)->diffForHumans() : '' }}
+                        <a href="{{ url('admin/videos/remind/' . $video->alpha_id ) }}" class="btn btn-primary btn-danger">
+                            Send Reminder
+                        </a>
                     @else
                         More Details
-                        Requested: {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$video->more_details_sent)->diffForHumans() }}
-                        <a href="{{ url('admin/videos/remind/'.$video->alpha_id ) }}"
-                           class="btn btn-primary btn-danger">Send Reminder</a>
+                        Requested: {{ ($video->more_details_sent) ? \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $video->more_details_sent)->diffForHumans() : '' }}
+                        <a href="{{ url('admin/videos/remind/' . $video->alpha_id ) }}" class="btn btn-primary btn-danger">
+                            Send Reminder
+                        </a>
                     @endif
                 </div>
-                <a href="{{ url('admin/videos/status/rejected/'.$video->alpha_id ) }}"
-                   class="btn btn-primary btn-danger">Reject</a>
+                <a href="{{ url('admin/videos/status/rejected/' . $video->alpha_id ) }}" class="btn btn-primary btn-danger">
+                    Reject
+                </a>
             @elseif($video->state == 'rejected')
-                <a href="{{ url('admin/videos/status/accepted/'.$video->alpha_id ) }}"
-                   class="btn btn-primary btn-success">Accept</a>
+                <a href="{{ url('admin/videos/status/accepted/' . $video->alpha_id ) }}" class="btn btn-primary btn-success">
+                    Accept
+                </a>
             @endif
 
             @if($video->file)
-                <a href="{{ url('/download/'.$video->alpha_id) }}"
-                   class="btn btn-primary{{ $video->file_watermark ? ' js-download' : '' }}"
-                   title="Download Video" download><i class="fa fa-download"></i></a>
+                <a href="{{ url('/download/'.$video->alpha_id) }}" class="btn btn-primary{{ $video->file_watermark ? ' js-download' : '' }}" title="Download Video" download>
+                    <i class="fa fa-download"></i>
+                </a>
             @endif
-            <a href="{{ url('/admin/pdfview/'.$video->alpha_id) }}" class="btn btn-primary"
-               title="Download License" download><i class="fa fa-print"></i></a>
-            <a href="{{ url('/admin/nsfw/'.$video->alpha_id) }}" class="btn btn-primary"
-               title="Flag NSFW"><i class="fa fa-flag"></i></a>
+            <a href="{{ url('/admin/pdfview/'.$video->alpha_id) }}" class="btn btn-primary" title="Download License" download>
+                <i class="fa fa-print"></i>
+            </a>
+            <a href="{{ url('/admin/nsfw/'.$video->alpha_id) }}" class="btn btn-primary" title="Flag NSFW">
+                <i class="fa fa-flag"></i>
+            </a>
         </div>
 
         <div class="clearfix"></div>
