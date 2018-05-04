b2bee999a8ea0da2f1942304cb29a07d6e232e37
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-05-04 12:00:47 +0100

    Fixing mobile & tablet style issue

---
+++ resources/assets/frontend/scripts/vue-component/forms/IframeTestingComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <iframe src="http://f26924b8.ngrok.io/upload/form" width="1000" height="1000"></iframe>
+    </div>
+</template>
+<script>
+    export default {
+        date(){
+
+        },
+
+        methods :{
+
+        }
+    }
+</script>
