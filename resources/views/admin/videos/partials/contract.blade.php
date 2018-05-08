639cdea1d98cc517bf5bdb373ee15722780cef06
Francisco Perez <frank@unilad.co.uk>
2018-05-08 17:25:52 +0100

    Add login to contract tab to check if you can send a contract

--- resources/views/admin/videos/partials/contract.blade.php
+++ resources/views/admin/videos/partials/contract.blade.php
@@ -1,6 +1,13 @@
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
-    Create Contract
-</button>
-{{--
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Send Contract</button>
---}}
+@if((!$video->currentContract) && ($video->contact))
+    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#contract_modal">
+        Create Contract
+    </button>
+@elseif($video->contact)
+    <a href="{{ route('contract.send', ['id' => $video->id]) }}" class="btn btn-info btn-lg">
+        Send Contract
+    </a>
+@else
+    <p>
+        Can't create/send a contract if there the video is not assigned to a creator
+    </p>
+@endif
