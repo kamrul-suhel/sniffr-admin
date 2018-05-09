57f2fb8c6c6cc39b9f3ea5841b78ecd0bc117b95
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-05-09 11:26:55 +0100

    fixing airbreak error

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -17,11 +17,11 @@
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
             h._hjSettings={hjid:874669,hjsv:6};
-            a=o.getElementsByTagName(‘head’)[0];
-            r=o.createElement(‘script’);r.async=1;
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
-        })(window,document,‘http://static.hotjar.com/c/hotjar-‘,’.js?sv=’);
+        })(window,document,‘https://static.hotjar.com/c/hotjar-‘,’.js?sv=’);
     </script>
 
     <!-- If we need to add page specific style -->
