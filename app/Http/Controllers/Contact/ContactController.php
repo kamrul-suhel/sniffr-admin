aa783e6d614b4c58455a64f1e8a13046fde85e08
Francisco Perez <frank@unilad.co.uk>
2018-05-15 12:42:01 +0100

    Correct ContactController Namespace

--- app/Http/Controllers/Contact/ContactController.php
+++ app/Http/Controllers/Contact/ContactController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Contact;
 
 use App\Http\Requests\Contact\CreateContactRequest;
 use App\Http\Requests\Contact\UpdateContactRequest;
