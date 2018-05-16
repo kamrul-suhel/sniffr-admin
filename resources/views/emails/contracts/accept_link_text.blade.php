5ea6c50aec68316ab1d6b5f2a3cba7e6573d5512
Francisco Perez <frank@unilad.co.uk>
2018-05-16 17:03:38 +0100

    Remember to modify the text email with the contract landing page link

--- resources/views/emails/contracts/accept_link_text.blade.php
+++ resources/views/emails/contracts/accept_link_text.blade.php
@@ -2,15 +2,8 @@ Hey {{ $video->contact->full_name }}!
 
 Video Ref: {{ $video->alpha_id }}
 
-Thanks for sending over more details about your video, and confirming this with us!
-
-We’re really keen to get going with this so be sure to keep an eye out on the page to see if/when your video is featured.
-
-We may also allow some other Facebook pages to use the video to ensure it has the best chance of going viral. If you see any uploads you’re unsure of, please do send them our way and we’ll check them!
-
-If you would like to submit any more content to us, you can do so by following this link: {{ url('/upload/') }}
-
-If you have any questions you’re more than welcome to contact the team here: submissions@unilad.co.uk
+If you would like to submit any more content to us, you can do so by following this link:
+{{ route('contract.accept', ['contract_id' => $contract->token]) }}
 
 Regards,
 
