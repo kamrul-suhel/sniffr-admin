6db2fd1fd708d845b4daefb00a482ffaa5b48811
Francisco Perez <frank@unilad.co.uk>
2018-05-23 15:26:23 +0100

    Add story relationship to Order

--- app/Order.php
+++ app/Order.php
@@ -5,21 +5,33 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property integer story_id
+ * @property integer user_id
+ * @property integer client_id
+ * @property array|string ip_address
+ * @property array|string user_agent
+ */
 class Order extends Model
 {
     use SoftDeletes;
     protected $table = 'orders';
     protected $guarded = [];
     public static $rules = [];
-    protected $fillable = ['user_id', 'video_id', 'client_id', 'ip', 'user_agent', 'ordered_at'];
+    protected $fillable = [
+        'user_id',
+        'story_id',
+        'client_id',
+        'ip_address',
+        'user_agent',
+    ];
 
     public function client()
     {
         return $this->belongsTo(Client::class)->first();
     }
 
-
-    public function video()
+    public function story()
     {
         return $this->belongsTo(Video::class)->first();
     }
