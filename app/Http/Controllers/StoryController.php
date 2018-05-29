a6c9a93504d5ca5cca59c30558bf82d0785ecc0e
Francisco Perez <frank@unilad.co.uk>
2018-05-29 12:42:31 +0100

    Add Story zip download for client path

---
+++ app/Http/Controllers/StoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Asset;
+use App\Story;
+use App\Video;
+use Chumper\Zipper\Facades\Zipper;
+use Illuminate\Http\Request;
+
+class StoryController extends Controller
+{
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function download($id)
+    {
+        $story = Story::find($id);
+        if (!$story) {
+            abort(404, 'Story Not Found');
+        }
+
+        $videos = Video::with('stories')->whereHas('stories', function ($query) use ($id) {
+            $query->where('stories.id', '=', $id);
+        })->get();
+
+        $assets = Asset::with('stories')->whereHas('stories', function ($query) use ($id) {
+            $query->where('stories.id', '=', $id);
+        })->get();
+
+        if ((!$assets->count()) && (!$videos->count())) {
+            abort(404, 'No Assets Found in this Story');
+        }
+
+        $newZipFileName = 'public/zips/'.time().'.zip';
+        $prefix = 'sniffr_';
+        $files = [];
+
+        if ($assets->count()) {
+            foreach ($assets as $asset) {
+                $info = pathinfo($asset->url);
+                $ext = $info['extension'];
+
+                $tempImage = tempnam(sys_get_temp_dir(), $prefix) . '.' . $ext;
+                copy($asset->url, $tempImage);
+                $files[] = $tempImage;
+            }
+        }
+
+        if ($videos->count()) {
+            foreach ($videos as $video) {
+                $info = pathinfo($video->file);
+                $ext = $info['extension'];
+
+                $tempVideo = tempnam(sys_get_temp_dir(), $prefix) . '.' . $ext;
+                copy($video->file, $tempVideo);
+                $files[] = $tempVideo;
+            }
+        }
+
+        Zipper::make($newZipFileName)->add($files)->close();
+        return response()->download($newZipFileName);
+    }
+}
