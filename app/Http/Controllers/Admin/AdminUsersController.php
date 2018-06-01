6006406bf56858e5df3db5ce6675bf636070bb11
Michael Wright <mike.filmworks@gmail.com>
2018-06-01 12:08:07 +0100

    more fixes to users controller

--- app/Http/Controllers/Admin/AdminUsersController.php
+++ app/Http/Controllers/Admin/AdminUsersController.php
@@ -77,7 +77,7 @@ class AdminUsersController extends Controller
         $user->password = Hash::make($request->input('password'));
         $user->role = $request->input('role');
         $user->active = $request->input('active');
-        $user->client_id = $request->input('client_id');
+        $user->client_id = ($request->input('client_id') ? $request->input('client_id') : NULL);
 
         $user->avatar = 'default.jpg';
 
@@ -130,7 +130,9 @@ class AdminUsersController extends Controller
 
         $user->role = $request->input('role', $user->role);
         $user->active = $request->input('active', $user->active);
-        $user->client_id = $request->input('client_id', $user->client_id);
+        if($user->client_id) {
+             $user->client_id = $request->input('client_id', $user->client_id);
+        }
 
         if ($request->hasFile('avatar')) {
             $user->avatar = ImageHandler::uploadImage($request->file('avatar'), 'avatars');
