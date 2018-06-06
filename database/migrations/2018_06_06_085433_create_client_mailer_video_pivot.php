ed8627bc5107dc73387d17b937bc4dadb00fba9f
Francisco Perez <frank@unilad.co.uk>
2018-06-06 10:12:39 +0100

    Create Client Mailer - Videos relationship Migration + Model Method

---
+++ database/migrations/2018_06_06_085433_create_client_mailer_video_pivot.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientMailerVideoPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('client_mailer_video', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_mailer_id')->unsigned()->index();
+            $table->integer('video_id')->unsigned()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('client_mailer_video', function (Blueprint $table) {
+            Schema::drop('client_mailer_video');
+        });
+    }
+}
