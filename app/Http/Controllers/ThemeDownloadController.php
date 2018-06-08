e792d4218982aadaaabb977a092e44efb9eb7992
Ian Lainchbury <ianlainchbury@gmail.com>
2018-06-08 16:15:38 +0100

    Adding contract signings to email queue
    Also handle dollars in contract

--- app/Http/Controllers/ThemeDownloadController.php
+++ app/Http/Controllers/ThemeDownloadController.php
@@ -13,7 +13,7 @@ class ThemeDownloadController extends Controller
 {
 	public function __construct()
 	{
-		//$this->middleware('auth');
+		$this->middleware('auth');
 	}
 
     public function index($id, $type = 'watermark')
