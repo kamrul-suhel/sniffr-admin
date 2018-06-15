69865e84ffb2491241256bb6e01f81e9ad6bdbcc
Michael Wright <mike.filmworks@gmail.com>
2018-06-15 10:52:22 +0100

    After testing, fixing mistakes

--- resources/views/admin/videos/partials/copy.blade.php
+++ resources/views/admin/videos/partials/copy.blade.php
@@ -18,11 +18,11 @@
             </div>
         </div>
 
-        @if(Auth::user()->role == 'admin' && Auth::user()->role == 'manager')
+        @if($video->state == 'licensed' && (Auth::user()->role == 'admin' || Auth::user()->role == 'manager'))
         <div class="form-group">
             <div class="input-group">
                 <span class="input-group">Make Featured</span>
-                <input type="checkbox" @if(isset($video->featured) && $video->featured==1)checked="checked" value="1"@else value="0"@endif name="featured" id="featured" />
+                <input type="checkbox" @if(isset($video->featured) && $video->featured==1)checked="checked"@endif value="1" name="featured" id="featured" />
             </div>
         </div>
         @endif
