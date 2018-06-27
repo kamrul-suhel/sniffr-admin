0ba55e93cd01f52ae8272f62994703652d494e10
Hemm Kerrai <hemmit.kerrai@unilad.co.uk>
2018-06-27 11:11:41 +0100

    create email queue, and template for Request Pending

---
+++ app/Mail/Quotes/PendingQuote.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail\Quotes;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PendingQuote extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.quotes.pending')
+            ->subject('SNIFFR - Your Request is being reviewed');
+    }
+}
