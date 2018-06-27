090d105347e7e1204ac709a8aced4877225452b7
Michael Wright <mike.filmworks@gmail.com>
2018-06-27 15:28:40 +0100

    working on stories index

--- resources/views/admin/stories/index.blade.php
+++ resources/views/admin/stories/index.blade.php
@@ -2,6 +2,55 @@
 
 
 @section('content')
+
+<style>
+select.btn-mini {
+    height: auto;
+    line-height: 14px;
+}
+select.btn {
+    -webkit-appearance: button;
+       -moz-appearance: button;
+            appearance: button;
+    padding-right: 0px;
+}
+
+select.btn-mini + .caret {
+    margin-left: -20px;
+    margin-top: 9px;
+}
+.btn-mini-border {
+    padding:2px 4px;
+    background-color: #f9f9f9;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+}
+.btn-mini-border.left {
+    margin-right:8px;
+}
+.panel-options {
+    margin-bottom: 0px !important;
+    padding: 5px;
+}
+.panel-options-body {
+    margin: 10px 5px;
+    padding: 0px;
+    text-align: center;
+}
+.title-overlay {
+    padding:15px;
+    color:#fff;
+}
+.title-overlay h4 {
+    background-color: rgba(0, 0, 0, 0.4);
+    padding: 8px;
+    width: 90%;
+    border-radius: 4px;
+}
+</style>
+
 	<div class="admin-section-title bottom-padding">
 		<div class="row">
 			<div class="col-xs-12">
@@ -63,20 +112,56 @@
 						<header>
                             <div class="video-container">
                                 <a class="video-thumb" href="#" style="background-image:url({{ ($story->thumb ? $story->thumb : '/assets/frontend/images/placeholder.png') }})">
-                                    <span class="thumbnail-overlay"></span>
+                                    <span class="thumbnail-overlay title-overlay">
+                                        <h4>{{ $story->title }}</h4>
+                                    </span>
                                 </a>
                             </div>
 						</header>
 
 						<section class="album-info">
-							<h3><a href="#">{{ $story->title }}</a></h3>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <p>{!! $story->excerpt !!}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="panel panel-primary panel-options" data-collapsed="0">
+                                        <div class="panel-options-body">
+                                            @if($story->state == 'unapproved')
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-success js-state approved btn-mini" title="Approve"><i class="fa fa-check"></i> Approve</a>
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-danger js-state rejected" title="Reject"><i class="fa fa-times"></i></a>
+                                            @elseif($story->state == 'approved')
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-success js-state licensing" title="Pick Up"><i class="fa fa-check"></i></a>
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-danger js-state rejected" title="Reject"><i class="fa fa-times"></i></a>
+                                            @elseif($story->state == 'licensing')
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-success js-state licensed" title="License"><i class="fa fa-check"></i></a>
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-danger js-state unlicensed" title="Unlicensed"><i class="fa fa-times"></i></a>
+                                            @elseif($story->state == 'licensed')
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-success js-state published btn-mini btn-mini-border left" title="Publish"><i class="fa fa-check"></i> Publish</a>
+                                                <a href="#" data-id="{{ $story->alpha_id }}" class="text-danger js-state unlicensed btn-mini btn-mini-border" title="Unlicense"><i class="fa fa-times"></i></a>
+                                            @else
 
-							<p>{!! $story->excerpt !!}</p>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="panel panel-primary panel-options" data-collapsed="0">
+                                        <div class="panel-options-body">
+                                            <select id="state" name="state" class="btn btn-mini" title="State">
+                                                @foreach(config('stories.states') as $current_state)
+                    							<option value="{{ $current_state }}" @if($story->state==@$current_state) selected @endif>{{ ucwords(str_replace('-', ' ', $current_state)) }}</option>
+                                                @endforeach
+                    						</select>
+                                            <span class="caret"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 						</section>
 
-						<footer>
-
-						</footer>
 					</article>
 				</div>
 			@endforeach
