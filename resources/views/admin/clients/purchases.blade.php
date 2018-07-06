f44128399bd6712de50590e28910cb00c4110764
Ian Lainchbury <ianlainchbury@gmail.com>
2018-07-06 11:32:26 +0100

    Tidying up purchases table

--- resources/views/admin/clients/purchases.blade.php
+++ resources/views/admin/clients/purchases.blade.php
@@ -33,6 +33,8 @@
 
         <table class="table table-striped pages-table">
             <tr class="table-header">
+                <th>Thumb</th>
+                <th>Aplha ID</th>
                 <th>Order No.</th>
                 <th>Order Date</th>
                 <th>Story / Video</th>
@@ -44,27 +46,31 @@
                 @foreach($collection->collectionVideos as $purchasedVideo)
                 <tr>
                     <td>
+                        <img src="{{ $purchasedVideo->video->thumb }}" width="100px">
+                    </td>
+
+                    <td>
+                        {{ $purchasedVideo->video->alpha_id }}
+                    </td>
+
+                    <td>
                         {{ $collection->name }} {{ $purchasedVideo->id }}
                     </td>
+
                     <td>
                         {{ date('jS M Y H:i:s',strtotime($collection->updated_at)) }}
                     </td>
-                    <td>
-                        <div class="row">
-                            <div class="col-xs-4">
-                                <img src="{{ $purchasedVideo->video->thumb }}" width="100%">
-                            </div>
 
-                            <div class="col-xs-8">
-                                <h3 class="title"><a target="_blank" href="{{ url('admin/videos/edit/'.$purchasedVideo->video->alpha_id) }}">{{ $purchasedVideo->video->title }} <i class="fa fa-external-link"></i></a></h3>
-                            </div>
-                        </div>
+                    <td>
+                        <a target="_blank" href="{{ url('admin/videos/edit/'.$purchasedVideo->video->alpha_id) }}">{{ $purchasedVideo->video->title }} <i class="fa fa-external-link"></i></a>
                     </td>
+
                     <td>
                         {{ $downloads->where('video_id', $purchasedVideo->video->id)->count() }}
                     </td>
+
                     <td>
-                        £ {{ $purchasedVideo->final_price }}
+                        £{{ $purchasedVideo->final_price }}
                     </td>
                 </tr>
                 @endforeach
@@ -74,29 +80,33 @@
                 @foreach($collection->collectionStories as $purchasedStory)
                 <tr>
                     <td>
+                        <img src="{{ $purchasedStory->story->thumb }}" width="100px">
+                    </td>
+
+                    <td>
+                        {{ $purchasedStory->story->alpha_id }}
+                    </td>
+
+                    <td>
                         {{ $collection->name }}  {{ $purchasedStory->story->id }}
                     </td>
+
                     <td>
                         {{ date('jS M Y H:i:s',strtotime($collection->updated_at)) }}
                     </td>
-                    <td>
-                        <div class="row">
-                            <div class="col-xs-4">
-                                <img src="{{ $purchasedStory->story->thumb }}" width="100%">
-                            </div>
 
-                            <div class="col-xs-8">
-                                <h3 class="title"><a target="_blank" href="{{ url('admin/stories/edit/'.$purchasedStory->story->alpha_id) }}">{{ $purchasedStory->story->title }} <i class="fa fa-external-link"></i></a></h3>
-                            </div>
-                        </div>
+                    <td>
+                        <a target="_blank" href="{{ url('admin/stories/edit/'.$purchasedStory->story->alpha_id) }}">{{ $purchasedStory->story->title }} <i class="fa fa-external-link"></i></a>
                     </td>
+
                     <td>
                         {{ $downloads->where('story_id', $purchasedStory->story->id)->count() }}
                     </td>
+
                     <td>
-                        £ {{ $purchasedStory->final_price }}
+                        £{{ $purchasedStory->final_price }}
                     </td>
-                    </tr>
+                </tr>
                 @endforeach
             @endforeach
         </table>
