ed808dc83c6394d04c4b46f1ef85f79e1dad1bb9
Hemm <hemmit.kerrai@unilad.co.uk>
2018-07-09 16:25:36 +0100

    fixes to offered quote email
    hides license terms if it's a story. updated image for stories

--- resources/views/emails/quotes/offered.blade.php
+++ resources/views/emails/quotes/offered.blade.php
@@ -9,28 +9,30 @@
     <div class="col-lg-12">
         <div class="col-lg-6">
             <h2>{{ $data['collectionAsset']->{$data['type']}->title }}</h2>
-            <img style="width:50%;" src="{{$data['collectionAsset']->{$data['type']}->image}}"/>
+            <img style="width:50%;" src="{{$data['collectionAsset']->{$data['type']}->image ?? $data['collectionAsset']->{$data['type']}->thumb }}"/>
         </div>
-        <div class="col-lg-6">
-            <p>With the following terms:</p>
-            <ul>
-                <li>
-                    <p>Video Name:
-                        <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->{$data['type']}->title)) }}</b>
-                    </p>
-                </li>
-                <li>
-                    <p>Type of License: <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->type)) }}</b></p>
-                </li>
-                <li>
-                    <p>Platform Usage: <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->platform)) }}</b>
-                    </p>
-                </li>
-                <li>
-                    <p>License Length: <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->length)) }}</b></p>
-                </li>
-            </ul>
-        </div>
+        @if($data['type'] == 'Video')
+            <div class="col-lg-6">
+                <p>With the following terms:</p>
+                <ul>
+                    <li>
+                        <p>Video Name:
+                            <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->{$data['type']}->title)) }}</b>
+                        </p>
+                    </li>
+                    <li>
+                        <p>Type of License: <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->type)) }}</b></p>
+                    </li>
+                    <li>
+                        <p>Platform Usage: <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->platform)) }}</b>
+                        </p>
+                    </li>
+                    <li>
+                        <p>License Length: <b>{{ ucwords(str_replace('-', ' ', $data['collectionAsset']->length)) }}</b></p>
+                    </li>
+                </ul>
+            </div>
+        @endif
     </div>
     <br>
     <h1>Our quote price is: <strong>£{{ $data['quote'] }}</strong></h1>
