770f7b72c0ba9dc61449f9c01a6fefa7948037ee
Ian Lainchbury <ianlainchbury@gmail.com>
2018-07-12 15:40:06 +0100

    Adding in extra graph to show licensed videos and who licesnsed them

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -35,6 +35,15 @@ class DashboardController extends Controller
 	        return Carbon::parse($date->created_at)->format('m-d'); // grouping by days
 	    });
 
+		$licensed_chaser_videos = Video::get()->where('rights', 'exc')->where('licensed_at', '>', (new Carbon)->now()->subDays(30))->groupBy(function($date) {
+			return Carbon::parse($date->licensed_at)->format('m-d'); // grouping by days
+		});
+
+		foreach ($licensed_chaser_videos as $video) {
+			echo count($video->currentContract->where('user_id', 1)) . ',';
+		}
+
+
 		// TODO: is this date arbitrary? is it a period?
 	    $video_state_count = Video::get()->where('created_at', '>', $this->startDate)
             ->groupBy('state');
@@ -46,6 +55,7 @@ class DashboardController extends Controller
 			'video_state_count' => $video_state_count,
 			'total_videos' => $videos->count(),
 			'videos' => $all_videos,
+			'licensed_chaser_videos' => $licensed_chaser_videos,
 			'new_videos' => (!$videos_by_state['new']) ? 0 : $videos_by_state['new']->count(),
 			'licensed_videos' => (!$videos_by_state['licensed']) ? 0 : $videos_by_state['licensed']->count(),
 			'pending_videos' => (!$videos_by_state['pending']) ? 0 : $videos_by_state['pending']->count(),
--- resources/views/admin/dashboard/partials/js.blade.php
+++ resources/views/admin/dashboard/partials/js.blade.php
@@ -152,47 +152,67 @@
             }
         });
 
-        var ctx = $('#sub-state-overview');
+        var ctx = $('#exc-licensed-breakdown');
         ctx.height(285);
-        new Chart($("#sub-state-overview"), {
-            "type": "doughnut",
+        new Chart($("#exc-licensed-breakdown"), {
+            "type": "bar",
             "data": {
-                "labels": ['Accepted', 'Licensed', 'Restricted', 'Problem', 'Rejected'],
-                "datasets": [{
-                    "data": [
-                        <?php echo count($video_state_count['accepted']); ?>,
-                        <?php echo count($video_state_count['licensed']); ?>,
-                        <?php echo count($video_state_count['restricted']); ?>,
-                        <?php echo count($video_state_count['problem']) ?>,
-                        <?php echo count($video_state_count['rejected']) ?>
-                    ],
-                    "backgroundColor": [
-                        "rgb(166, 255, 172)",
-                        "rgb(0, 160, 90)",
-                        "rgb(255, 205, 86)",
-                        "rgb(255, 80, 80)",
-                        "rgb(255, 20, 20)",
-                    ]
-                }]
+                "labels": [
+					<?php foreach ($licensed_chaser_videos as $video) {
+					echo '"' . $video[0]->created_at->format('jS M') . '",';
+				}?>
+                ],
+                "datasets": [
+                    {
+                        "label": 'New',
+                        "data": [
+							<?php foreach ($licensed_chaser_videos as $video) {
+							echo count($video->currentContract->where('user_id', 1)) . ',';
+						}?>
+                        ],
+                        "fill": false,
+                        "backgroundColor": "rgba(54, 162, 235, 0.2)",
+                        "borderWidth": 1
+                    },
+                    {
+                        "label": 'Pending',
+                        "data": [
+							<?php foreach ($licensed_chaser_videos as $video) {
+							echo count($video->where('state', 'pending')) . ',';
+						}?>
+                        ],
+                        "fill": false,
+                        "backgroundColor": "rgba(255, 205, 86, 0.2)",
+                        "borderWidth": 1
+                    },
+                    {
+                        "label": 'Licensed',
+                        "data": [
+							<?php foreach ($licensed_chaser_videos as $video) {
+							echo count($video->currentContract()->where('user_id', 1)) . ',';
+						}?>
+                        ],
+                        "fill": false,
+                        "backgroundColor": "rgba(0, 160, 90, 0.2)",
+                        "borderWidth": 1
+                    }
+                ]
             },
             "options": {
                 maintainAspectRatio: false,
                 legend: {
                     display: true
                 },
-                tooltips: {
-                    callbacks: {
-                        label: function (tooltipItem, data) {
-                            var dataset = data.datasets[tooltipItem.datasetIndex];
-                            var total = dataset.data.reduce(function (previousValue, currentValue, currentIndex, array) {
-                                return previousValue + currentValue;
-                            });
-
-                            var currentValue = dataset.data[tooltipItem.index];
-                            var precentage = Math.floor(((currentValue / total) * 100) + 0.5);
-                            return currentValue + ' (' + precentage + "%)";
+                "scales": {
+                    "xAxes": [{
+                        stacked: true
+                    }],
+                    "yAxes": [{
+                        stacked: true,
+                        "ticks": {
+                            "beginAtZero": true
                         }
-                    }
+                    }]
                 }
             }
         });
