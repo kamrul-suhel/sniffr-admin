fba6faaeed4bcf740668c67b3412c360fdb8ff52
Ian Lainchbury <ianlainchbury@gmail.com>
2018-07-18 15:21:23 +0100

    Need to add contact on update

--- app/Http/Controllers/Admin/AdminStoryController.php
+++ app/Http/Controllers/Admin/AdminStoryController.php
@@ -269,7 +269,7 @@ class AdminStoryController extends Controller
             'decision' => $decision,
             'user' => Auth::user(),
             'users' => User::all(),
-			'contact' => null,
+			'contact' => $asset->contact,
             'video_categories' => VideoCategory::all(),
             'video_collections' => VideoCollection::all(),
             'note' => 'Successfully Saved Story!',
