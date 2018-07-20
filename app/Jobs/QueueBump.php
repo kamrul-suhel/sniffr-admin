3c178bd8d9152e8049bbd6e898072a0d61600bdc
Ian Lainchbury <ianlainchbury@gmail.com>
2018-07-20 17:47:27 +0100

    Trying to fix tweet message dupe

--- app/Jobs/QueueBump.php
+++ app/Jobs/QueueBump.php
@@ -57,23 +57,18 @@ class QueueBump implements ShouldQueue
 				preg_match('/\/([\d]+)/', $asset->source, $matches);
 				$tweetId = $matches[1];
 
-				switch ($asset->reminders) {
-					case '':
-						$dmMessage = 'Hey! how are you? I\'m a senior writer from UNILAD and would love to talk to you about your tweet for an article. Do you have some time to talk to me today? ' . $from .' Ref: '.$asset->alpha_id;
-						$replyMessage = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. Could you DM me or email stories@unilad.co.uk :) Ref: '.$asset->alpha_id;
-						$replyMessageDmSuccess = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. Could you DM me or email stories@unilad.co.uk :) Ref: '.$asset->alpha_id;
-						break;
-					case 0:
-						$dmMessage = 'Hey! Would be great to chat. Do you have some time today? Ref: '.$asset->alpha_id;
-						$replyMessage = 'Hello again ' . $twitterHandle . '! UNILAD here, I would love to have a chat with you. Could you DM me :) Ref: '.$asset->alpha_id;
-						$replyMessageDmSuccess = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. I’ve just sent you a DM! Ref: '.$asset->alpha_id;
-						break;
-					case 1:
-					default:
-						$dmMessage = 'Hey, did you get my message? :) Ref: '.$asset->alpha_id;
-						$replyMessage = 'Hey ' . $twitterHandle . '! Could you DM me :) Ref: '.$asset->alpha_id;
-						$replyMessageDmSuccess = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. I’ve just DM\'d you! Ref: '.$asset->alpha_id;
-						break;
+				if($asset->reminders == 1){
+					$dmMessage = 'Hey, did you get my message? :) Ref: '.$asset->alpha_id;
+					$replyMessage = 'Hey ' . $twitterHandle . '! Could you DM me :) Ref: '.$asset->alpha_id;
+					$replyMessageDmSuccess = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. I’ve just DM\'d you! Ref: '.$asset->alpha_id;
+				}elseif($asset->reminders == 0){
+					$dmMessage = 'Hey! Would be great to chat. Do you have some time today? Ref: '.$asset->alpha_id;
+					$replyMessage = 'Hello again ' . $twitterHandle . '! UNILAD here, I would love to have a chat with you. Could you DM me :) Ref: '.$asset->alpha_id;
+					$replyMessageDmSuccess = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. I’ve just sent you a DM! Ref: '.$asset->alpha_id;
+				}else{
+					$dmMessage = 'Hey! how are you? I\'m a senior writer from UNILAD and would love to talk to you about your tweet for an article. Do you have some time to talk to me today? ' . $from .' Ref: '.$asset->alpha_id;
+					$replyMessage = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. Could you DM me or email stories@unilad.co.uk :) Ref: '.$asset->alpha_id;
+					$replyMessageDmSuccess = 'Hey ' . $twitterHandle . '! It’s ' . $from . ' from UNILAD and I would love to have a chat with you. Could you DM me or email stories@unilad.co.uk :) Ref: '.$asset->alpha_id;
 				}
 
 				$success = true;
