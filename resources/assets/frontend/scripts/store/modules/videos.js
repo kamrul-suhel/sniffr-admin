fa324fd8a7cd6ac617bb76da8a643722a6209060
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-07-31 15:35:20 +0100

    Fixing store video & video services.

--- resources/assets/frontend/scripts/store/modules/videos.js
+++ resources/assets/frontend/scripts/store/modules/videos.js
@@ -1,3 +1,4 @@
+import AssetVideoServices from '../../services/VideoServices'
 const state = {
     //Dialog box
     videoDialogBox: false,
@@ -79,13 +80,6 @@ const getters = {
 
     getVideoLoading(state){
         return state.videoLoading;
-    },
-
-
-
-
-    getCurrentVideo(state) {
-        return state.currentVideo;
     },
 
     getCurrentVideoAlphaId() {
@@ -246,20 +240,7 @@ const mutations = {
      */
     setOfferedVideos(state, videos){
 
-        // IAN: Need to convert it to an arrray if it returns an object, for some stupid reason the pagination returns an object
-        if (typeof videos.data == 'object') {
-            videos.data = Object.values(videos.data);
-        }
-        let allVideos = [];
-        videos.data.forEach((video) => {
-            video[0].video.final_price = video[0].final_price;
-            video[0].video.platform = video[0].platform;
-            video[0].video.type = video[0].type;
-            video[0].video.length = video[0].length;
-            video[0].video.collection_video_id = video[0].id;
-            video[0].video.collection_status = video[0].status;
-            allVideos.push(video[0].video);
-        });
+        let allVideos = AssetVideoServices.processVideoData(videos)
         state.offeredVideos = allVideos;
     },
 
@@ -270,21 +251,8 @@ const mutations = {
      * ***************************************
      */
     setPurchasedVideos(state, videos){
-        if (typeof videos.data == 'object') {
-            videos.data = Object.values(videos.data);
-        }
-        let allVideos = [];
-        videos.data.forEach((video) => {
-            video[0].video.final_price = video[0].final_price;
-            video[0].video.platform = video[0].platform;
-            video[0].video.type = video[0].type;
-            video[0].video.length = video[0].length;
-            video[0].video.collection_video_id = video[0].id;
-            video[0].video.collection_status = video[0].status;
-            allVideos.push(video[0].video);
-        });
-
-        state.purchasedVideos = allVideos;
+        let allVideos = AssetVideoServices.processVideoData(videos)
+        state.purchasedVideos = allVideos
     },
 
     setInitVideo(state, value){
@@ -353,6 +321,11 @@ const actions = {
             data.value = state.current_route_obj.query.value
         }
 
+        if (state.current_route_obj.query.type != 'undefined' && state.current_route_obj.query.type === 'offered') {
+            data.offered = true;
+        }
+        let url = '/search/videos';
+
         axios.post('/search/videos', data)
             .then((response) => {
                 commit('setCurrentVideo', response.data.current_video);
@@ -418,6 +391,49 @@ const actions = {
                 },
                 (error) => {
                 });
+    },
+
+    fetchOfferedDialogNextPrevious({commit, state}) {
+        let currIndex = state.assetOfferedCurrentIndex;
+        let allVideos = state.offeredVideos;
+        let currentAlphaId = '';
+        let previousAlphaId = '';
+        let nextAlphaId = '';
+        let currentVideoPosition = currIndex;
+        let totalVideos = allVideos.length - 1;
+        let hasNextPage = state.paginate.last_page;
+
+        if (allVideos[Object.keys(allVideos)[currIndex]]) {
+            currentAlphaId = allVideos[Object.keys(allVideos)[currIndex]].alpha_id;
+        }
+
+        if (allVideos[Object.keys(allVideos)[currIndex - 1]]) {
+            previousAlphaId = allVideos[Object.keys(allVideos)[currIndex - 1]].alpha_id;
+        }
+
+        if (allVideos[Object.keys(allVideos)[currIndex + 1]]) {
+            nextAlphaId = allVideos[Object.keys(allVideos)[currIndex + 1]].alpha_id;
+        }
+
+        if (currentVideoPosition === totalVideos && hasNextPage) {
+            // if has next page, do next page fetch the data
+        }
+
+        state.previousVideoAlphaId = previousAlphaId;
+        state.currentVideoAlphaId = currentAlphaId;
+        state.nextVideoAlphaId = nextAlphaId;
+
+        // now fetch the data form server
+        let data = {alpha_id: currentAlphaId};
+        let url = '/search/videos';
+
+        axios.post(url, data)
+            .then((response) => {
+                let currVideo = response.data.current_video;
+                commit('setCurrentVideo', currVideo)
+                commit('setVideoLoading', false)
+            });
+
     }
 
 
