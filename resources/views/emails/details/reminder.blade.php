5e3b52b14e446842d2178d399c4b80231979160c
Hemm <hemmit.kerrai@unilad.co.uk>
2018-08-06 15:25:14 +0100

    email updates for Adam

--- resources/views/emails/details/reminder.blade.php
+++ resources/views/emails/details/reminder.blade.php
@@ -15,17 +15,13 @@
 
 <div>Thanks again for submitting your video to us!</div>
 
-<div>We&rsquo;ve received no response from you yet to verify your video and confirm your submission.</div>
-
-<div>Please answer the online questionnaire below, <strong>*we cannot feature your video if you do not fill out this form*</strong>:</div>
-
-<div>&nbsp;</div>
+<div>We&rsquo;ve received no response from you yet to verify your video and confirm your submission. Please answer the online questionnaire below.</div>
 
 <div><a href="{{ url('/details/' . $video->more_details_code) }}">Confirm additional details</a></div>
 
 <div>&nbsp;</div>
 
-<div>We&rsquo;re really keen to use your video so please fill this in as soon as possible! Once you have done so,&nbsp; our team will review the content and get in touch with further opportunities for you!</div>
+<div>We’re really keen to use your video so please fill this in as soon as possible!</div>
 
 <div>&nbsp;</div>
 
