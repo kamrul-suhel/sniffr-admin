0b7292193454ac20a09364c7fe8659847d864e3e
Hemm <hemmit.kerrai@unilad.co.uk>
2018-08-08 15:31:25 +0100

    Added negotiations to quotes. Updated prcing array for lengths

--- app/Http/Requests/Quote/CreateQuote.php
+++ app/Http/Requests/Quote/CreateQuote.php
@@ -33,6 +33,10 @@ class CreateQuote extends FormRequest
             $rules = [];
         }
 
+        if(request()->has('license_type')) {
+            $rules = [];
+        }
+
         return $rules;
     }
 }
