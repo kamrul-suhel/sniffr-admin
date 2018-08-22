0e77afa3cf6852b2272fc975590b59b0d044b931
Ian Lainchbury <ianlainchbury@gmail.com>
2018-08-22 12:23:27 +0100

    Adding in reporting to wp story syn

--- app/Jobs/QueueStory.php
+++ app/Jobs/QueueStory.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\Traits\WordpressAPI;
 
+use App\User;
 use App\Story;
 use App\Asset;
 use App\Libraries\VideoHelper;
@@ -53,7 +54,11 @@ class QueueStory implements ShouldQueue
             if(isset($story)&&!$story->wp_id) {
                 $parameters = 'title='.urlencode($story->title).'&content='.urlencode($story->description).'&tags='.env('UNILAD_WP_TAG_ID');
                 $result = $this->apiPost('posts', $parameters, true);
-                // update stories record with WP response from post
+
+				$user = new User();
+				$user->slackChannel('alerts')->notify(new SubmissionAlert('WP Update (Result: ' . json_encode($result). ')'));
+
+				// update stories record with WP response from post
                 if($result->id){
                     $story->wp_id = $result->id;
                     $story->status = $result->status;
