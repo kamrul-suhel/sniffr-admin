7bacca90b3055f4e44aabbd9ee82445a6848b13b
Hemm <hemmit.kerrai@unilad.co.uk>
2018-08-28 11:10:10 +0100

    Using airbrake official package. updated handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,9 @@
 
 namespace App\Exceptions;
 
+use Airbrake\ErrorHandler;
+use Airbrake\Instance;
+use Airbrake\Notifier;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
@@ -36,10 +39,17 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-    	if(app()->environment() === 'prod') {
+    	if(app()->environment() === 'production') {
 			if ($this->shouldReport($exception)) {
-				$airbrakeNotifier = \App::make('Airbrake\Notifier');
-				$airbrakeNotifier->notify($exception);
+				$airbrakeNotifier = new Notifier(['projectId' => config('airbrake.projectId'), 'projectKey' => config('airbrake.projectKey')]);
+				Instance::set($airbrakeNotifier);
+				$handler = new ErrorHandler($airbrakeNotifier);
+				$handler->register();
+				try {
+					$airbrakeNotifier->notify($exception);
+				} catch (Exception $e) {
+					\Log::error($e->getMessage());
+				}
 			}
 		}
 
