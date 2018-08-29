8e8ae7b306c13f3858f083d9985b67aee6d4910c
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-08-29 10:01:31 +0100

    Fixing Stories & button font

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,11 @@ module.exports = laravelNuxt({
                 rel: 'icon',
                 type: 'image/x-icon',
                 href: '/favicon.ico'
+            },
+
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
             }
         ],
 
