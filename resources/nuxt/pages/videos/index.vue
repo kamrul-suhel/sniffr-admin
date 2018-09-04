cc14827549d69f72217fdd87a54bce43974c12c4
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-09-04 15:24:06 +0100

    Ignore public/_nuxt & .nuxt folder from git.

--- resources/nuxt/pages/videos/index.vue
+++ resources/nuxt/pages/videos/index.vue
@@ -13,34 +13,32 @@
 
             <v-container grid-list-lg>
                 <v-layout row wrap>
-                    <v-flex xs12 md3>
+                    <v-flex xs12 md3 xl2>
                         <search-component/>
                     </v-flex>
 
-                    <v-flex xs12 md9>
-                            <v-layout row wrap v-if="client_logged_in && Object.keys(mailerVideos).length > 0">
-                                <v-flex xs12 class="text-center">
-                                    <h2 class="text-uppercase">Your Suggested Videos</h2>
-                                    <p class="mb-0 ">We've gone ahead and procured a list of videos we think you will love!</p>
-                                </v-flex>
+                    <v-flex xs12 md9 xl10>
+                        <v-layout row wrap v-if="client_logged_in && Object.keys(mailerVideos).length > 0">
+                            <v-flex xs12 class="text-center">
+                                <h2 class="text-uppercase">Your Suggested Videos</h2>
+                                <p class="mb-0 ">We've gone ahead and procured a list of videos we think you will love!</p>
+                            </v-flex>
 
-                                <v-flex xs12>
-                                    <v-layout align-content-center style="overflow-x:scroll;" class="mb-4">
-                                        <video-loop-component
-                                                v-for="(mailer, index) in mailerVideos"
-                                                :video="mailer"
-                                                :key="mailer.alpha_id"
-                                                :type="'suggest'"
-                                                :index="index"
-                                                :width="'350px'"
-                                        ></video-loop-component>
-                                    </v-layout>
-                                </v-flex>
-                            </v-layout>
+                            <v-flex xs12>
+                                <v-layout align-content-center style="overflow-x:scroll;" class="mb-4">
+                                    <video-loop-component
+                                            v-for="(mailer, index) in mailerVideos"
+                                            :video="mailer"
+                                            :key="mailer.alpha_id"
+                                            :type="'suggest'"
+                                            :index="index"
+                                            :width="'350px'"
+                                    ></video-loop-component>
+                                </v-layout>
+                            </v-flex>
+                        </v-layout>
 
-
-
-                            <v-layout row wrap>
+                        <v-layout row wrap>
                                 <video-loop-component
                                         v-for="(video, index) in videos"
                                         :video="video"
