6473028b55dce1d0312d2cc8b4b5b742f905d982
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-09-11 17:48:56 +0100

    Implementing search reset button.

--- resources/nuxt/components/search/index.vue
+++ resources/nuxt/components/search/index.vue
@@ -5,6 +5,13 @@
         <search-by-tags/>
         <sort-by/>
         <!--<search-by-location/>-->
+
+        <v-btn dark
+               class="mt-4 mx-0 text-xs-right"
+               raised
+               @click="onResetSearchOption()">
+            Clear search
+        </v-btn>
     </section>
 </template>
 <script>
@@ -14,6 +21,7 @@
     import SearchByTags from './SearchByTags'
     import SearchByLocation from './SearchByLocation'
     import SortBy from './SortBy'
+    import SearchServices from '@/plugins/services/SearchServices';
     export default {
 
         components: {
@@ -24,20 +32,11 @@
             SortBy
         },
 
-        data() {
-            return {
-
-            }
-        },
-
-        props: [
-            'searchOption'
-        ],
-
-        created() {
-        },
-
         methods: {
+            onResetSearchOption(){
+                this.$store.commit('resetSearchStoreObject');
+                SearchServices.updateSearchRoute(this.$route, this.$router, this.$store.getters.getSearchQueryUrl);
+            }
         }
     }
 </script>
