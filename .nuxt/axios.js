d73df717fc66cbe54f4ca6adf2a6a814b624cae7
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-09-13 21:13:39 +0100

    Axios https request not work.

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -113,8 +113,8 @@ export default (ctx, inject) => {
   const axiosOptions = {
     // baseURL
     baseURL : process.browser
-      ? '/api'
-      : (process.env._AXIOS_BASE_URL_ || '/api'),
+      ? 'https://www.sniffr-app.test/api'
+      : (process.env._AXIOS_BASE_URL_ || 'https://www.sniffr-app.test/api'),
 
     // Create fresh objects for all default header scopes
     // Axios creates only one which is shared across SSR requests!
