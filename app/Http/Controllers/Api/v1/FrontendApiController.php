aac6dd226a35610f6f70923b7821d97329a6d5c2
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-09-20 15:16:01 +0100

    Adding base controller for api.

---
+++ app/Http/Controllers/Api/v1/FrontendApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FrontendApiController extends Controller
+{
+    public function __construct()
+    {
+    }
+}
